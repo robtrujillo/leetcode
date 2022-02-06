549ebfc279d8d22e7f1d4c1ee5b58c4e16b57844
Robert Trujillo <rvtrujillo1026@gmail.com>
2022-02-06 14:51:34 -0500

    Time: 33 ms (41.03%), Space: 79.8 MB (7.93%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -1,14 +1,13 @@
 class Solution {
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
-        // 2 different ways of retrieving max gaps
-        int[] currentGaps = new int[heights.length];
+        // int[] currentGaps = new int[heights.length];
         Queue<Integer> brickAllocations = new PriorityQueue<>((a, b) -> b - a);
         if(ladders >= heights.length - 1) {
             return heights.length-1;
         }
         for(int i = 0; i < heights.length - 1; i++) {
             int bricksNeeded = heights[i+1] - heights[i];
-            currentGaps[i] = bricksNeeded;
+            // currentGaps[i] = bricksNeeded;
             if(bricksNeeded <= 0) {
                 // dont need to use bricks/ladders
                 continue;
@@ -23,22 +22,16 @@ class Solution {
             
             // replace largest gap with ladder
             if (bricks < 0) {
-                ladders--;
-                int largestGapSoFar = bricksNeeded;
+                // int largestGapSoFar = bricksNeeded;
                 // for(int j=0; j <= i; j++) {
-                //     if (currentGaps[j] > 0 && currentGaps[j] > largestGapSoFar) {
-                //         largestGapSoFar = currentGaps[j];
-                //         currentGaps[j] = 0;
-                //     }
+                    // if (currentGaps[j] > 0 && currentGaps[j] >= largestGapSoFar) {
+                        // largestGapSoFar = currentGaps[j];
+                        // currentGaps[j] = -1;
+                    // }
                 // }
-                int removedFromQueue = brickAllocations.remove();
-                if(removedFromQueue != largestGapSoFar) {
-                    // System.out.println("Largest found from queue=" + removedFromQueue + " | from array=" + largestGapSoFar);
-                }
-              
-                // using the queue works, using the largestGapSoFar variable doesnt
+                ladders--;
                 // bricks += largestGapSoFar;
-                bricks += removedFromQueue;
+                bricks += brickAllocations.remove();
             }
         }
         // made it to the end
