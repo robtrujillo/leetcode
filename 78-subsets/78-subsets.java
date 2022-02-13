66f5248918cca1b2979c4cfe8f8e29d198ed9c1d
Robert Trujillo <rvtrujillo1026@gmail.com>
2022-02-13 12:18:56 -0500

    Time: 1 ms (77.93%), Space: 43.6 MB (8.40%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,23 +1,23 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ret = new ArrayList<>();
+        List<List<Integer>> masterList = new ArrayList<>();
         // start off with 1 entry of an empty set
-        ret.add(new ArrayList<>());
+        masterList.add(new ArrayList<>());
         for(int i = 0; i < nums.length; i++) {
             int num = nums[i];
-            int currentRetLength = ret.size();
+            int currentMasterLength = masterList.size();
             // only loop the inner lists up to the current length
             // since the length will increase as we are adding to it
             // and we dont want to add subsets like [2,2]
-            for(int j=0; j < currentRetLength; j++) {
+            for(int j=0; j < currentMasterLength; j++) {
                 // make a copy of each inner list
-                List<Integer> copy = new ArrayList<>(ret.get(j));
+                List<Integer> copy = new ArrayList<>(masterList.get(j));
                 // add the new num to the copy of the inner list
                 copy.add(num);
                 // add the new subset to the master list we are returning
-                ret.add(copy);
+                masterList.add(copy);
             }
         }
-        return ret;
+        return masterList;
     }
 }
