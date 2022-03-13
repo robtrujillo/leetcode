5bf4ef5936a925d625899c9552fa5a4d3cedf39a
Robert Trujillo <rvtrujillo1026@gmail.com>
2022-03-13 15:22:20 -0400

    Time: 3 ms (56.34%), Space: 42 MB (36.84%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,14 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
+        // LIFO data structure (last in, first out)
         ArrayDeque<Character> stack = new ArrayDeque<>();
         
+        // loop the string
         for(int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
+            // check if ending bracket
             if(c == ')') {
+                // if stack is empty, there was not an opening bracket and invalid string
                 if(stack.isEmpty()) {
                     return false;
                 }
+                // get last opening bracket found on stack
                 char open = stack.pop();
+                // if not a matching bracket, invalid string
                 if(open != '(') {
                     return false;
                 }
@@ -29,9 +35,11 @@ class Solution {
                     return false;
                 }
             } else {
+                // must be an opening bracket, add to stack
                 stack.push(c);
             }
         }
-        return stack.size() == 0;
+        // at end, all opening brackets should have been popped by their ending brackets and stack should be empty
+        return stack.isEmpty();
     }
 }
