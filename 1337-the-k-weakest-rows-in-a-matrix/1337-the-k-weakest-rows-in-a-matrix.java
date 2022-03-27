25e9a12af888b901c557e9c12a3fcaf318b6be0b
Robert Trujillo <rvtrujillo1026@gmail.com>
2022-03-27 14:31:19 -0400

    Time: 4 ms (37.99%), Space: 48.9 MB (31.65%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,24 +1,31 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
+        // keep track of their original indexes since we are sorting them
+        // need original indexes for the return
         Map<int[], Integer> originalIndexes = new HashMap<>();
         for(int i = 0; i < mat.length; i++) {
             originalIndexes.put(mat[i], i);
         }
-        
+        // assume all inner arrays are already sorted
+        // assume all inner arrays are equal in length
         Arrays.sort(mat, (rowOne, rowTwo) -> {
+            // sort in ascending order
             for(int i = 0; i < rowOne.length; i++) {
                 if(rowOne[i] < rowTwo[i]) {
                     // rowTwo has a soldier and rowOne a civ
+                    // so rowOne is lesser
                     return -1;
                 } else if(rowOne[i] > rowTwo[i]) {
                     // rowOne has a soldier and rowTwo a civ
+                    // so rowOne is greater
                     return 1;
                 } else {
                     // they are equal (both soldiers, or both civs)
+                    // keep checking for when they differ
                     continue;
                 }
             }
-            // they are equal in soliders, so rowTwo is bigger by default
+            // they are equal in soliders, so rowTwo is greater by default
             return 1;
         });
         
